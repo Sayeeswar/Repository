3434e906db4e2d6bb44790a30441f10146cdfecc
Sayeeswar <sayeeswar2004@gmail.com>
2025-08-09 12:15:48 +0530

    comiited

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@
 
 # Register your models here.
 from django.contrib import admin
-from .models import Author, Book
+from .models import Author,Book
 
 admin.site.register(Author)
 admin.site.register(Book)
--- project/settings.py
+++ project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'SSSIHMS',
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
